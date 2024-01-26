a0fbf05424d161d0f7639ae7e5b38779a328901e
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-26 21:16:50 +0100

    Added changes to function

--- functions.php
+++ functions.php
@@ -1,13 +1,14 @@
 <?php 
-function random_password($password_length, $characters, $pass){
-    if(isset($_GET['password'])){
-        for ($i=0; $i<$password_length; $i++) {
-            $carattere_casuale = array_rand($characters, 1);
-            array_push($pass,$characters[$carattere_casuale]);
-        }
-        for ($i=0; $i<count($pass); $i++) {
-            echo $pass[$i];
-        }
+function random_password($password_length){
+    $caratteri = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"];
+    $password = [];
+    for ($i=0; $i<$password_length; $i++) {
+        $carattere_casuale = array_rand($caratteri, 1);
+        array_push($password, $caratteri[$carattere_casuale]);
     }
+    for ($i=0; $i<count($password); $i++) {
+        echo $password[$i];
+    }
+    
 }
 ?>
--- index.php
+++ index.php
@@ -3,10 +3,8 @@ include __DIR__.'/functions.php';
 
 if(isset($_GET['password'])){
     $lunghezza_password = $_GET['password'];
-    $caratteri = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"];
-    $password = [];
     echo '<h3>Password: <h3>';
-    echo '<h2>'.random_password($lunghezza_password, $caratteri, $password). '</h2>';
+    echo '<h2>'.random_password($lunghezza_password). '</h2>';
 }
 ?>
 
